fe3f801e425130295bb6b4f0e44db41b24145500
Baran <barandasdemir.bd@gmail.com>
2025-07-19 12:19:08 +0300

    asenkron_methodlar

--- StoreFlowEntityFramework/Controllers/OrderController.cs
+++ StoreFlowEntityFramework/Controllers/OrderController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using StoreFlowEntityFramework.Context;
+using StoreFlowEntityFramework.Entities;
 using StoreFlowEntityFramework.ViewComponents;
+using System.Security.Cryptography.X509Certificates;
 
 namespace StoreFlowEntityFramework.Controllers
 {
@@ -68,6 +72,93 @@ namespace StoreFlowEntityFramework.Controllers
             var values2 = _context.Orders.ToList();
             return View(values2);
         }
+
+
+
+
+
+
+        public async Task<IActionResult> OrderListAsync2()
+        {
+            var values = await _context.Orders.Include(x => x.Product).Include(y => y.Customer).ToListAsync();
+            return View(values);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateOrder()
+        {
+
+            var products = await _context.Products.Select(x => new SelectListItem
+            {
+                Value = x.ProductId.ToString(),
+                Text = x.ProductName
+            }).ToListAsync();
+            ViewBag.product = products;
+
+
+            var customer = await _context.Customers.Select(x => new SelectListItem
+            {
+                Value = x.CustomerId.ToString(),
+                Text = x.CustomerName + " " + x.CustomerSurname
+            }).ToListAsync();
+
+            ViewBag.customer = customer;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(Order order)
+        {
+            order.Status = "Sipariş Alındı";
+            order.OrderDate = DateTime.Now;
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("OrderListAsync2");
+        }
+
+
+        public async Task<IActionResult> DeleteOrder(int id)
+        {
+            var detectedValues = await _context.Orders.FindAsync(id);
+             _context.Orders.Remove(detectedValues);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("OrderListAsync2");
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateOrder(int id)
+        {
+            var product = await _context.Products.Select(x => new SelectListItem
+            {
+                Value = x.ProductId.ToString(),
+                Text = x.ProductName
+            }).ToListAsync();
+
+            var customer = await _context.Customers.Select(c => new SelectListItem
+            {
+                Value = c.CustomerId.ToString(),
+                Text = c.CustomerName + " " + c.CustomerSurname
+            }).ToListAsync();
+
+            ViewBag.product = product;
+            ViewBag.customer = customer;
+
+            var detectedValues = await _context.Orders.FindAsync(id);
+            return View(detectedValues);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateOrder(Order order)
+        {
+            _context.Orders.Update(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("OrderListAsync2");
+
+        }
+
+
 
 
 
